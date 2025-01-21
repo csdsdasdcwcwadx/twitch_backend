eab63324df91292ecfa00ab5259e28d9d83813c0
Jack.Tsai <Jack.Tsai@acti.com>
2025-01-21 17:44:46 +0800

    新增twitch token 加入判斷

--- src/Controllers/user.ts
+++ src/Controllers/user.ts
@@ -17,20 +17,20 @@ const login = async (req: Request, res: Response) => {
     try {
         const tokenResponse = await axios.post("https://id.twitch.tv/oauth2/token", null, {
             params: {
-            client_id: clientId,
-            client_secret: clientSecret,
-            code,
-            grant_type: "authorization_code",
-            redirect_uri: redirectUri,
+                client_id: clientId,
+                client_secret: clientSecret,
+                code,
+                grant_type: "authorization_code",
+                redirect_uri: redirectUri,
             },
         });
     
-        const twitchAccessToken = tokenResponse.data.access_token;
+        const twitchToken = tokenResponse.data.access_token;
         
         // 2. 使用 Access Token 獲取 Twitch 使用者資料
         const userResponse = await axios.get("https://api.twitch.tv/helix/users", {
             headers: {
-                "Authorization": `Bearer ${twitchAccessToken}`,
+                "Authorization": `Bearer ${twitchToken}`,
                 "Client-Id": clientId,
             },
         });
@@ -57,6 +57,12 @@ const login = async (req: Request, res: Response) => {
                 maxAge: 24*60*60*1000,
                 domain: cookieDomain,
             })
+            res.cookie('twitch', twitchToken, {
+                httpOnly: true,
+                secure: true,
+                maxAge: 24*60*60*1000,
+                domain: cookieDomain,
+            })
             if (result.userinfo[0].isAdmin) {
                 res.redirect(`${domainEnv}:3000/back`);
             } else {
@@ -70,24 +76,44 @@ const login = async (req: Request, res: Response) => {
 }
 
 const logout = async (req: Request, res: Response) => {
-    res.clearCookie('refresh', {
-        httpOnly: true,
-        secure: true,
-        domain: cookieDomain,
-    });
-    res.clearCookie('access', {
-        httpOnly: true,
-        secure: true,
-        domain: cookieDomain,
-    });
-    if (process.env.Env === "prod") {
-        res.redirect(`${domainEnv}:3000`);
-        return;
+    const clientId = process.env.TWITCH_CLIENT_ID;
+    const twitchToken = req.cookies.twitch;
+
+    try {
+        const logoutResult = await axios.post(`https://id.twitch.tv/oauth2/revoke?client_id=${clientId}&token=${twitchToken}`);
+        if (logoutResult.status === 200) {
+            res.clearCookie('refresh', {
+                httpOnly: true,
+                secure: true,
+                domain: cookieDomain,
+            });
+            res.clearCookie('access', {
+                httpOnly: true,
+                secure: true,
+                domain: cookieDomain,
+            });
+            res.clearCookie('twitch', {
+                httpOnly: true,
+                secure: true,
+                domain: cookieDomain,
+            });
+            if (process.env.Env === "prod") {
+                res.redirect(`${domainEnv}:3000`);
+                return;
+            }
+            res.json({
+                status: true,
+                href: `${domainEnv}:3000`,
+            })
+            return;   
+        }
+        throw new Error("登出失敗");
+    } catch(e) {
+        res.json({
+            status: false,
+            message: "登出失敗",
+        })
     }
-    res.json({
-        status: true,
-        href: `${domainEnv}:3000`,
-    })
 }
 
 export {
